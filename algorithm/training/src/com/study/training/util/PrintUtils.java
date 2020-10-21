d46d230c5050218950e622723702857f4d519116
giyeon95 <giyeon15@gmail.com>
2020-10-21 23:33:24 +0900

    update PrintUtils

--- algorithm/training/src/com/study/training/util/PrintUtils.java
+++ algorithm/training/src/com/study/training/util/PrintUtils.java
@@ -55,6 +55,15 @@ public class PrintUtils {
         }
     }
 
+    public static void print(boolean[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static <T> void print(T[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
