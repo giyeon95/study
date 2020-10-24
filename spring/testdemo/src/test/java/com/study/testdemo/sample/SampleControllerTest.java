f430f8e26bc386b3669aadc8fe14752c8113378d
giyeon95 <giyeon15@gmail.com>
2020-10-24 22:59:28 +0900

    TestRestTemplete MockBean

--- spring/testdemo/src/test/java/com/study/testdemo/sample/SampleControllerTest.java
+++ spring/testdemo/src/test/java/com/study/testdemo/sample/SampleControllerTest.java
@@ -1,6 +1,7 @@
 package com.study.testdemo.sample;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -19,8 +21,13 @@ class SampleControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() throws Exception {
+        when(mockSampleService.getName()).thenReturn("kiyeon_kim");
+
         String result = testRestTemplate.getForObject("/hello", String.class);
         assertThat(result).isEqualTo("hello kiyeon_kim");
     }
