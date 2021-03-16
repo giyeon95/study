ef8ce4ab4e9890ffd06781a5039306618af53ce5
kiyeon_kim1 <kiyeon_kim1@kolon.com>
2021-03-16 09:08:32 +0900

    toby-spring daily study

--- spring/toby-spring/src/main/java/com/study/spring/user/repository/UserRepository.java
+++ spring/toby-spring/src/main/java/com/study/spring/user/repository/UserRepository.java
@@ -12,8 +12,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 
 public abstract class UserRepository {
 
-    //    @Setter
-//    private ConnectionMaker connectionMaker;
     @Setter
     private DataSource dataSource;
 
@@ -26,12 +24,15 @@ public abstract class UserRepository {
     }
 
     public void add(User user) throws SQLException {
-        Connection c = dataSource.getConnection();
+        jdbcContextWithStatementStrategy(c -> {
+            PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) "
+                + "values (?, ?, ?)");
 
-        StatementStrategy strategy = new AddStatement(user);
-
-
-        jdbcContextWithStatementStrategy(strategy);
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
+            return ps;
+        });
 
     }
 
@@ -78,47 +79,18 @@ public abstract class UserRepository {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy strategy = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(strategy);
+        jdbcContextWithStatementStrategy(c ->
+            c.prepareStatement("delete from users")
+        );
     }
 
     public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
         try (Connection c = dataSource.getConnection();
-            PreparedStatement ps = stmt.makePreparedStatement(c)){
+            PreparedStatement ps = stmt.makePreparedStatement(c)) {
 
             ps.executeUpdate();
         } catch (SQLException e) {
             throw e;
         }
     }
-
-    @Deprecated
-    public void originalDeleteAll() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("delete from users");
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-
-                }
-            }
-        }
-    }
-
 }
