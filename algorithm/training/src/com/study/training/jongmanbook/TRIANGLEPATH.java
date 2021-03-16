9d2595d7822bd6a77f2ba4b522dce798906c0c47
giyeon95 <giyeon15@gmail.com>
2021-03-16 23:15:21 +0900

    Algorithm (jmbook)

--- algorithm/training/src/com/study/training/jongmanbook/TRIANGLEPATH.java
+++ algorithm/training/src/com/study/training/jongmanbook/TRIANGLEPATH.java
@@ -1,13 +1,13 @@
 package com.study.training.jongmanbook;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 // 난이도 하
 public class TRIANGLEPATH {
 
     public static int[][] input;
+    public static int[][] d;
     public static int line;
 
     public static void main(String[] args) {
@@ -15,32 +15,42 @@ public class TRIANGLEPATH {
         int n = sc.nextInt();
 
         int[] anw = new int[n];
+        int[] timer = new int[n];
 
         int a = 0;
         while (n-- > 0) {
             line = sc.nextInt();
             input = new int[line][line];
+            d = new int[line][line];
+
+            for (int[] item : d) {
+                Arrays.fill(item, -1);
+            }
 
             for (int i = 0; i < line; i++) {
                 for (int j = 0; j <= i; j++) {
                     input[i][j] = sc.nextInt();
                 }
             }
+            anw[a++] = path(0, 0);
 
-            anw[a++] = path(0, 0, 0);
         }
 
         Arrays.stream(anw).boxed().forEach(System.out::println);
 
     }
 
-    public static int path(int y, int x, int sum) {
+    public static int path(int y, int x) {
         if (y == line - 1) {
-            return input[y][x] + sum;
+            return input[y][x];
         }
 
-        sum += input[y][x];
+        if (d[y][x] != -1) {
+            return d[y][x];
+        }
 
-        return Math.max(path(y + 1, x + 1, sum), path(y + 1, x, sum));
+        d[y][x] = Math.max(path(y + 1, x + 1), path(y + 1, x)) + input[y][x];
+
+        return d[y][x];
     }
 }
