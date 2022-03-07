5125c37d1c35db197bd08603c7864630f1236ca3
giyeon95 <giyeon15@gmail.com>
2022-03-07 22:01:36 +0900

    baekjoon daily study Q_18108

---
+++ algorithm/training/src/com/study/training/baek/Q_18108.java
@@ -0,0 +1,12 @@
+package com.study.training.baek;
+
+import java.util.Scanner;
+
+public class Q_18108 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(input - 543);
+    }
+}
